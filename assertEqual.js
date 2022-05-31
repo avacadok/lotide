8ed6569645e52cc9d9cd7fcf5f3130d9fad3d6df
AVacAdok <avakuang8@gmail.com>
2022-05-31 03:28:24 +0000

    corrected code

--- assertEqual.js
+++ assertEqual.js
@@ -2,7 +2,7 @@
 const assertEqual = function(actual, expected) {
 
   if (actual !== expected) {
-      console.log(`❌❌❌Assertion Failed: ${actual} === ${expected}`)
+      console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`)
 
       //can also use console.assert()
       //console.assert(actual === expected,( actual + " !== " +  expected))
