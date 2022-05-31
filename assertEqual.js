87c8c78a5575ba75f2c263414c80faaba68f02f3
AVacAdok <avakuang8@gmail.com>
2022-05-31 02:17:46 +0000

    console.assert() can determine if the statement is true or false, msg will be printed if the statement is not true

---
+++ assertEqual.js
@@ -0,0 +1,23 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+
+  if (actual !== expected) {
+      console.log("Assertion Failed: " + actual + " === " + expected)
+
+      //can also use console.assert()
+      //console.assert(actual === expected,( actual + " !== " +  expected))
+      //msg will be printed if actual !== expected
+
+  } else {
+      console.log("Assertion Passed: " + actual + " === " + expected)
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(5, 8);
+assertEqual("Snowie is sleeping", "Snowie is sleeping");
+
+
