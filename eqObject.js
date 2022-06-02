c4318a2ebf76adc5af218df98372aa6dc24f63f9
AVacAdok <avakuang8@gmail.com>
2022-06-02 21:06:46 +0000

    polished code and works

--- eqObject.js
+++ eqObject.js
@@ -40,33 +40,35 @@ const eqObjects = function(object1, object2) {
     return false;
   }
   //only use for ..in to loop through array;
-  for (let key of keys1) {
-
-    //values = (object[key])
+  for (let key in object1) {
+    //check if keys2 includes all the key in keys
+    // if (keys2.includes(key)){
+    //check if the value inside object1 and object2
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-
-      return eqArrays(object1[key], object2[key])
-
+      //compare both array's value
+      if (eqArrays(object1[key], object2[key]) !== true) {
+        return false;
+      }
     } else if (object1[key] !== object2[key]) {
-      
+
       return false;
     }
     
-  } return true; 
+  } return true;
 };
 
 //not sure how to do this -.-
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
+assertEqual(eqObjects(ab, ba), true); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
+assertEqual(eqObjects(ab, abc), false); // => false
 
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
+assertEqual(eqObjects(cd, dc), true); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false
+assertEqual(eqObjects(cd, cd2), false); // => false
