ca8881c8b50914cea64b3bcca01bcc2db98a7d0c
AVacAdok <avakuang8@gmail.com>
2022-06-02 05:19:36 +0000

    eslint file, edit spacing

--- without.js
+++ without.js
@@ -1,44 +1,3 @@
-
-//create function to compare if two array are the same
-const assertArraysEqual = function(arr1, arr2) {
-
-  //compare the length of the arrays are the same; if not we can console.log false msg here
-  if (arr1.length !== arr2.length) {
-
-    console.log(`❌❌❌Assertion Failed: ${arr1} !== ${arr2}`);
-    return false;
-    //use return the stop code keep running after msg logged
-  }
-
-  //use for loop the loop through all the element in the arrays and compare all the element for both array
-  for (let i = 0; i < arr1.length; i++) {
-
-    if (arr1[i] !== arr2[i]) {
-
-      console.log(`❌❌❌Assertion Failed: ${arr1} !== ${arr2}`);
-      return false;
-    }
-  }
-
-  console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}`);
-  return true;
-};
-
-const assertEqual = function(actual, expected) {
-
-  if (actual !== expected) {
-    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-
-    //can also use console.assert()
-    //console.assert(actual === expected,( actual + " !== " +  expected))
-    //msg will be printed if actual !== expected
-
-  } else {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  }
-
-};
-
 // FUNCTION IMPLEMENTATION
 const eqArrays = function(arr1, arr2) {
 
@@ -49,12 +8,22 @@ const eqArrays = function(arr1, arr2) {
   }
 
   for (let i = 0; i < arr1.length; i++) {
-    //ensure all the element in the array are the same;
+    //ensure all the element in the array are the same
     if (arr1[i] !== arr2[i]) {
       return false;
     }
   } return true;
 
+};
+//create function to compare if two array are the same based on the eqArray
+const assertArraysEqual = function(arr1, arr2) {
+  //refer to eqArray and console log the statement
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}`);
+
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${arr1} !== ${arr2}`);
+  }
 };
 
 //create function that remove msg for original array
