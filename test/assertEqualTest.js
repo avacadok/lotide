3f3e651acc31ab3ea6e74d2a43e35c159e71a114
AVacAdok <avakuang8@gmail.com>
2022-06-06 05:55:14 +0000

    refactor code

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(5, 8);
+assertEqual("Snowie is sleeping", "Snowie is sleeping");
