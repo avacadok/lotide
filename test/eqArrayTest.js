b38890e9d058ae410e81994f7a650788b8875a40
AVacAdok <avakuang8@gmail.com>
2022-06-06 17:06:45 +0000

    refactor code

---
+++ test/eqArrayTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require("../assertEqual");
+const eqArrays = require("../eqArrays");
+
+
+// TEST CODE
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2"], ["1", "2", "3"])); // => false
+console.log(eqArrays(["1", "2", 3], ["1", "2", "3"])); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
