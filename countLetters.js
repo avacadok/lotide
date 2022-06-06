c43cca8808e568369472f708d4d1fe30504c4df3
AVacAdok <avakuang8@gmail.com>
2022-06-06 22:40:16 +0000

    refactor code

--- countLetters.js
+++ countLetters.js
@@ -1,18 +1,4 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual !== expected) {
-    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-
-    //can also use console.assert()
-    //console.assert(actual === expected,( actual + " !== " +  expected))
-    //msg will be printed if actual !== expected
-
-  } else {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  }
-
-};
-
+const assertEqual = require("./assertEqual");
 
 const countLetters = function(strings) {
 
@@ -23,16 +9,14 @@ const countLetters = function(strings) {
     if (str !== " ") {
       //check if the str is already in the output or not
       if (output[str]) {
-
         output[str] += 1;
-        
       } else {
-
         output[str] = 1;
       }
     }
   } return output;
-
 };
 
 console.log(countLetters("Coding is so tough but so cooooool"));
+
+module.exports = countLetters;
