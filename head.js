0b946ae7bc0dc19159ecc015d538f9462b5df199
AVacAdok <avakuang8@gmail.com>
2022-06-06 05:56:57 +0000

    refactor code

--- head.js
+++ head.js
@@ -1,29 +1,7 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-
-  if (actual !== expected) {
-      console.log(`Assertion Failed: ${actual} !== ${expected}`)
-
-      //can also use console.assert()
-      //console.assert(actual === expected,( actual + " !== " +  expected))
-      //msg will be printed if actual !== expected
-
-  } else {
-      console.log(`Assertion Passed: ${actual} === ${expected}`)
-  }
-
-};
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(5, 8);
-assertEqual("Snowie is sleeping", "Snowie is sleeping");
-
+const assertEqual = require("./assertEqual")
 
 let head = function (arrs){
     return arrs[0];
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
